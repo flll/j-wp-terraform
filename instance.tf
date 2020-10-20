2ef1b393111f2bd042b4d1e3dcaed33912a7c879
lll <flll@users.noreply.github.com>
2020-10-21 04:20:26 +0900

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,11 +2,11 @@ resource "oci_core_instance" "test_instance" {
   count               = var.num_instances
   availability_domain = data.oci_identity_availability_domain.ad.name
   compartment_id      = var.compartment_ocid
-  display_name        = var.instance_display_name
-  shape               = var.instance_shape
+  display_name        = "var.instance_display_name"
+  shape               = "var.instance_shape"
   source_details {
     source_id = var.instance_image_ocid[var.region]
-    source_type = "image"
+    source_type = image
   }
   metadata = {
     ssh_authorized_keys = var.ssh_public_key
