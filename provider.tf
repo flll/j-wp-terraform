d2342f8c2ae6e24d4cd07fec1f07dee2e4f2a1bb
lll <flll@users.noreply.github.com>
2020-10-21 05:53:07 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,7 +3,6 @@ provider "oci" {
   user_ocid = "var.user_ocid"
   fingerprint = "var.fingerprint"
   private_key_path = "var.private_key_path"
-  region = "var.region"
 }
 
 output "InstancePublicIP" {
