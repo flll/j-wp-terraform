20c7c9e88e9c74dd46e7a81cdd987620cf8dc510
lll <flll@users.noreply.github.com>
2020-10-21 02:35:10 +0900

    Create instance-var.tf

---
+++ instance-var.tf
@@ -0,0 +1,14 @@
+variable "NumInstances"{
+  default = "1"
+}
+variable "instance_shape" {
+  default = "VM.Standard2.1"
+}
+variable "instance_image_ocid" {
+  type = "map"
+  default = {
+    ap-tokyo-1 = "ocid1.image.oc1.ap-tokyo-1.aaaaaaaa2lb55favhw7mhirylv4pniqu3oxpmoyjbrtfank4kdleiflfqbta"
+    ap-osaka-1 = "ocid1.image.oc1.ap-osaka-1.aaaaaaaaevxg6tpchgyijcdjlzcflkat5ndpsfy6n3tjois2qe3yrtrjrnlq"
+  }
+}
+variable "ssh_public_key" {}
