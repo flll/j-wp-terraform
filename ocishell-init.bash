5bfffa38bc1ecb3d27e97fd8ffe2a2b3a51040f6
lll <flll@users.noreply.github.com>
2020-10-21 03:32:08 +0900

    Update ocishell-init.bash

--- ocishell-init.bash
+++ ocishell-init.bash
@@ -13,7 +13,9 @@ if [ ! -e ~/.oci/oci-api-key.pem ]; then
         openssl rsa -pubout -in ~/.oci/oci-api-key.pem -out ~/.oci/oci-api-key-public.pem
         echo -n "done  "
 fi
-echo -n "init...  "
+
+
+echo -n "Initializing...  "
 export TF_VAR_compartment_ocid=`oci iam compartment list | jq -r '.data[].id'`
 export TF_VAR_tenancy_ocid=$OCI_TENANCY
 export IDentity_provider=`oci iam identity-provider list -c $TF_VAR_tenancy_ocid --protocol SAML2 | jq -r '.data[].id'`
@@ -22,7 +24,9 @@ export TF_VAR_private_key_path="~/.oci/oci-api-key.pem"
 export TF_VAR_fingerprint=`openssl rsa -pubout -outform DER -in ~/.oci/oci-api-key.pem | openssl md5 -c | sed -e 's/(stdin)= //'`
 export TF_VAR_region=$OCI_REGION
 export TF_VAR_ssh_public_key=`ssh-keygen -f ~/.oci/oci-api-key-public.pem -i -mPKCS8`
-echo -n "done  "
+echo -n "done"
+
+
 
 echo FINISH
 
