1caa576a6426c8c7ab05618fb5d900a7cbd15c87
lll <flll@users.noreply.github.com>
2020-10-21 08:40:16 +0900

    Update ocishell-init.bash

--- ocishell-init.bash
+++ ocishell-init.bash
@@ -29,4 +29,11 @@ echo -n "done"
 export -p
 echo FINISH
 
-. ./after/*.bash
+export TF_VAR_num_instances="1"
+export TF_VAR_instance_shape="VM.Standard2.1"
+export TF_VAR_instance_display_name="WordPressInstance"
+
+terraform init
+terraform plan -out="wp-p"
+terraform apply "wp-p"
+terraform output public_ip
