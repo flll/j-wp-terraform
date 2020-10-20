d6b5be88fed260a8229d87faac2e45eca01f8160
lll <flll@users.noreply.github.com>
2020-10-21 07:44:14 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,5 @@
 provider "oci" {
-  tenancy_ocid = var.tenancy_ocid
-  user_ocid = var.user_ocid
-  fingerprint = var.fingerprint
-  private_key_path = var.private_key_path
+  auth = "InstancePrincipal"
   region = var.region
 }
 
