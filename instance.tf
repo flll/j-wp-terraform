3a2d6fcfcf7b2121c1c64274f8c6d9d44e1aed85
lll <flll@users.noreply.github.com>
2020-10-21 07:59:59 +0900

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,10 +1,64 @@
+// Copyright (c) 2017, 2020, Oracle and/or its affiliates. All rights reserved.
+// Licensed under the Mozilla Public License v2.0
+
+variable "tenancy_ocid" {
+}
+
+variable "user_ocid" {
+}
+
+variable "fingerprint" {
+}
+
+variable "private_key_path" {
+}
+
+variable "region" {
+}
+
+variable "compartment_ocid" {
+}
+
+variable "ssh_public_key" {
+}
+
+variable "ssh_private_key" {
+}
+
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
+}
+# Defines the number of instances to deploy
+variable "num_instances" {
+  default = "3"
+}
+
+variable "num_iscsi_volumes_per_instance" {
+  default = "1"
+}
+
+variable "instance_shape" {
+  default = "VM.Standard2.1"
+}
+variable "flex_instance_image_ocid" {
+  type = map(string)
+  default = {
+    ap-tokyo-1 = "ocid1.image.oc1.ap-tokyo-1.aaaaaaaa2lb55favhw7mhirylv4pniqu3oxpmoyjbrtfank4kdleiflfqbta"
+    ap-osaka-1 = "ocid1.image.oc1.ap-osaka-1.aaaaaaaaevxg6tpchgyijcdjlzcflkat5ndpsfy6n3tjois2qe3yrtrjrnlq"
+  }
+}
+
 resource "oci_core_instance" "wp_instance" {
   compartment_id      = "var.compartment_ocid"
   display_name        = "var.instance_display_name"
   shape               = "var.instance_shape"
   source_details {
-    source_id = "var.instance_image_ocid[${var.region}]"
     source_type = "image"
+    source_id = var.flex_instance_image_ocid[var.region]
   }
   metadata = {
     ssh_authorized_keys = var.ssh_public_key
@@ -13,4 +67,9 @@ resource "oci_core_instance" "wp_instance" {
   timeouts {
     create = "60m"
   }
+}
+
+data "oci_identity_availability_domain" "ad" {
+  compartment_id = var.tenancy_ocid
+  ad_number      = 1
 }
