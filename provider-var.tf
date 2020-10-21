b968a0fb610911459fbd82fb07a728c1b9d661fc
lll <flll@users.noreply.github.com>
2020-10-21 16:54:43 +0900

    Create provider-var.tf

---
+++ provider-var.tf
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, 2020, Oracle and/or its affiliates. All rights reserved.
+// Licensed under the Mozilla Public License v2.0
+
+variable "tenancy_ocid" {}
+variable "region" {}
+variable "compartment_ocid" {}
+variable "ssh_public_key" {}
+variable "instance_display_name" {}
