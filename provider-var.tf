b631fd660e86cb094267ead40fa12fdd1c69ee4d
lll <flll@users.noreply.github.com>
2020-10-21 17:33:34 +0900

    Update provider-var.tf

--- provider-var.tf
+++ provider-var.tf
@@ -2,10 +2,10 @@
 // Licensed under the Mozilla Public License v2.0
 
 variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
+#variable "user_ocid" {}
+#variable "fingerprint" {}
+#variable "private_key_path" {}
 variable "region" {}
-variable "compartment_ocid" {}
+#variable "compartment_ocid" {}
 variable "ssh_public_key" {}
 variable "instance_display_name" {}
