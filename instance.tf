09d2ca679e9179ffc7c0014705a36506ff3627a4
lll <flll@users.noreply.github.com>
2020-10-21 16:54:48 +0900

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,16 +1,6 @@
 // Copyright (c) 2017, 2020, Oracle and/or its affiliates. All rights reserved.
 // Licensed under the Mozilla Public License v2.0
 
-variable "tenancy_ocid" {}
-variable "region" {}
-variable "compartment_ocid" {}
-variable "ssh_public_key" {}
-variable "instance_display_name" {}
-
-provider "oci" {
-  tenancy_ocid     = var.tenancy_ocid
-  region           = var.region
-}
 variable "num_instances" {
   default = "1"
 }
@@ -47,11 +37,4 @@ resource "oci_core_instance" "wp_instance" {
   timeouts {
     create = "60m"
   }
-}
-data "oci_identity_availability_domain" "ad" {
-  compartment_id = var.tenancy_ocid
-  ad_number      = 1
-}
-output "public_ip" {
-  value = ["oci_core_instance.wp_instance.*.public_ip"]
 }
