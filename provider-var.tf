29216c41fb68c2ca7b72ab1038d0eb4966460e77
lll <flll@users.noreply.github.com>
2020-10-22 07:54:33 +0900

    Update provider-var.tf

--- provider-var.tf
+++ provider-var.tf
@@ -1,6 +1,7 @@
 // Copyright (c) 2017, 2020, Oracle and/or its affiliates. All rights reserved.
 // Licensed under the Mozilla Public License v2.0
 
+variable "config_file_profile" {}
 variable "tenancy_ocid" {}
 #variable "user_ocid" {}
 #variable "fingerprint" {}
