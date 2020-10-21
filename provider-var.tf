a8b169e8e6608c91486493008d94eeee8c4a275f
lll <flll@users.noreply.github.com>
2020-10-21 17:34:34 +0900

    Update provider-var.tf

--- provider-var.tf
+++ provider-var.tf
@@ -6,6 +6,6 @@ variable "tenancy_ocid" {}
 #variable "fingerprint" {}
 #variable "private_key_path" {}
 variable "region" {}
-#variable "compartment_ocid" {}
+variable "compartment_ocid" {}
 variable "ssh_public_key" {}
 variable "instance_display_name" {}
