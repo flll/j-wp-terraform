e9410085b642237bdac7dda4c0ae5af8450a15e1
lll <flll@users.noreply.github.com>
2020-10-22 09:31:20 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,14 @@
 // Copyright (c) 2017, 2020, Oracle and/or its affiliates. All rights reserved.
 // Licensed under the Mozilla Public License v2.0
 
-# Configure the Oracle Cloud Infrastructure provider to use Instance Principal based authentication
-provider "oci" {
-  auth                = "SecurityToken"
-  config_file_profile = var.config_file_profile
-}
 
+provider "oci" {
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid = var.user_ocid
+  fingerprint = var.fingerprint
+  private_key_path = var.private_key_path
+  region = var.region
+}
 data "oci_identity_availability_domain" "ad" {
   compartment_id = var.tenancy_ocid
   ad_number      = 1
