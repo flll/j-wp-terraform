97f7226d5b5b3c87d1e3d202da7adae7f749193f
lll <flll@users.noreply.github.com>
2020-10-22 09:59:35 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,14 @@
 // Copyright (c) 2017, 2020, Oracle and/or its affiliates. All rights reserved.
 // Licensed under the Mozilla Public License v2.0
 
+provider "oci" {
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid = var.user_ocid
+  fingerprint = var.fingerprint
+  private_key_path = var.private_key_path
+  region = var.region
+}
+
 data "oci_identity_availability_domain" "ad" {
   compartment_id = var.tenancy_ocid
   ad_number      = 1
