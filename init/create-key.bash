af73eca21b734269da51d0db410883a6fc58b4d4
lll <flll@users.noreply.github.com>
2020-10-26 08:05:49 +0900

    Add files via upload

---
+++ init/create-key.bash
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+set -o pipefail
+cd `dirname $0`
+
+if [ ! -d ~/.oci ]; then
+    mkdir ~/.oci
+    echo -n "mkdir done  "
+fi
+if [ ! -e ~/.oci/oci-api-key.pem ]; then
+    echo -n "key-creating...  "
+    openssl genrsa -out ~/.oci/oci-api-key.pem 4096
+    chmod go-rwx ~/.oci/oci-api-key.pem
+    openssl rsa -pubout -in ~/.oci/oci-api-key.pem -out ~/.oci/oci-api-key-public.pem
+    echo -n "done  "
+fi
