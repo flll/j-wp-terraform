308f3c34e396a6f57df25c8fbac4b7f6fbea488a
lll <flll@users.noreply.github.com>
2020-10-26 08:03:39 +0900

    Update instance-create.bash

--- instance-create.bash
+++ instance-create.bash
@@ -2,17 +2,8 @@
 set -o pipefail
 cd `dirname $0`
 
-if [ ! -d ~/.oci ]; then
-    mkdir ~/.oci
-    echo -n "mkdir done  "
-fi
-if [ ! -e ~/.oci/oci-api-key.pem ]; then
-    echo -n "key-creating...  "
-    openssl genrsa -out ~/.oci/oci-api-key.pem 4096
-    chmod go-rwx ~/.oci/oci-api-key.pem
-    openssl rsa -pubout -in ~/.oci/oci-api-key.pem -out ~/.oci/oci-api-key-public.pem
-    echo -n "done  "
-fi
+#init
+. init/create-key.bash
 
 declare -A CLI_OCI_IMAGEMAP
 
@@ -21,11 +12,12 @@ CLI_OCI_IMAGEMAP=(
     ["ap-osaka-1"]="ocid1.image.oc1.ap-osaka-1.aaaaaaaaevxg6tpchgyijcdjlzcflkat5ndpsfy6n3tjois2qe3yrtrjrnlq"
 )
 export CLI_OCI_IMAGE=${CLI_OCI_IMAGEMAP[$OCI_REGION]}
+export CLI_OCI_COMPARTMENTID=`oci iam compartment list | jq -r '.data[]."compartment-id"'`
 
 oci compute instance launch \
-    --compartment-id `oci iam compartment list | jq -r '.data[]."compartment-id"'` \
+    --compartment-id $CLI_OCI_COMPARTMENTID \
     --shape VM.Standard2.1 \
-    --subnet-id `oci network subnet list  -c `oci iam compartment list | jq -r '.data[]."compartment-id"'` --sort-by TIMECREATED --sort-order ASC | jq -r '.data[0].id'` \
+    --subnet-id `oci network subnet list  -c $CLI_OCI_COMPARTMENTID --sort-by TIMECREATED --sort-order ASC | jq -r '.data[0].id'` \
     --assign-public-ip true \
     --display-name "Wordpress Instance" \
     --user-data-file "./cloud-config" \
