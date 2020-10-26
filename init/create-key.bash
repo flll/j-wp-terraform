7fd534f3d17d0a5eaf86b2a27d035db7276b17b8
lll <flll@users.noreply.github.com>
2020-10-26 10:11:55 +0900

    Update create-key.bash

--- init/create-key.bash
+++ init/create-key.bash
@@ -1,3 +1,14 @@
 #!/bin/bash
+if [ ! -d ~/.oci ]; then
+    mkdir ~/.oci
+    echo -n "mkdir done  "
+fi
+if [ ! -e ~/.oci/oci-api-key.pem ]; then
+    echo -n "key-creating...  "
+    openssl genrsa -out ~/.oci/oci-api-key.pem 4096
+    chmod go-rwx ~/.oci/oci-api-key.pem
+    openssl rsa -pubout -in ~/.oci/oci-api-key.pem -out ~/.oci/oci-api-key-public.pem
+    echo -n "done  "
+fi
 
 echo "done create-key"
