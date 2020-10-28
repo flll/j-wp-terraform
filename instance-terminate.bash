ba9ae4b31178f02e888e0362385e21865fb31cf8
lll <flll@users.noreply.github.com>
2020-10-28 17:00:13 +0900

    Update instance-terminate.bash

--- instance-terminate.bash
+++ instance-terminate.bash
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 set -o pipefail
 
+#最近作成されたインスタンスを削除する
 export CLI_OCI_COMPARTMENTID=`oci iam compartment list | jq -r '.data[]."compartment-id"'`
 export CLI_OCI_AD=`oci iam availability-domain list --compartment-id ${CLI_OCI_COMPARTMENTID} | jq -r '.data[].name'`
 export CLI_OCI_INSTANCE_OCID=`oci compute instance list --compartment-id ${CLI_OCI_COMPARTMENTID} --availability-domain ${CLI_OCI_AD} --sort-by TIMECREATED --sort-order DESC | jq -r '.data[0].id'`
 
-oci compute instance terminate --instance-id ${CLI_OCI_INSTANCE_OCID} 
-
+oci compute instance get --instance-id ${CLI_OCI_INSTANCE_OCID} --query "data[*].{\`"display-name\`":\`"display-name\`", shape:shape}" --output table
+#oci compute instance terminate --instance-id ${CLI_OCI_INSTANCE_OCID}
