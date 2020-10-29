644608f5f7a75eeac2f72e15db7b718be0401b62
lll <flll@users.noreply.github.com>
2020-10-29 21:10:14 +0900

    Update instance-launch.bash

--- instance-launch.bash
+++ instance-launch.bash
@@ -5,15 +5,13 @@ cd `dirname $0`
 #init
 bash init/create-key.bash
 
+echo -n "env Declaration... "
 declare -A CLI_OCI_IMAGEMAP
-
 CLI_OCI_IMAGEMAP=(
     ["ap-tokyo-1"]="ocid1.image.oc1.ap-tokyo-1.aaaaaaaacmmicmlaejkkorfp5es7r6h4hfi5zxupz3muxchksgkugztkl4ea"
     ["ap-osaka-1"]="ocid1.image.oc1.ap-osaka-1.aaaaaaaamcrmkxuvsk4coctz5jtsdbtoiin4xvvjo6zceonlib57eiliaupa"
 )
-
 export CLI_OCI_IMAGE=${CLI_OCI_IMAGEMAP[$OCI_REGION]}
-echo -n "env Declaration... "
 export CLI_OCI_COMPARTMENTID=`oci iam compartment list | jq -r '.data[]."compartment-id"'`
 export CLI_OCI_AD=`oci iam availability-domain list --compartment-id ${CLI_OCI_COMPARTMENTID} | jq -r '.data[].name'`
 echo "DONE"
