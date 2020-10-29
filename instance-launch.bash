b3efab150e5a19d175bc4ecee97519818fe0f886
lll <flll@users.noreply.github.com>
2020-10-29 18:00:25 +0900

    Update and rename instance-create.bash to instance-launch.bash

--- instance-launch.bash
+++ instance-launch.bash
@@ -9,13 +9,14 @@ declare -A CLI_OCI_IMAGEMAP
 
 CLI_OCI_IMAGEMAP=(
     ["ap-tokyo-1"]="ocid1.image.oc1.ap-tokyo-1.aaaaaaaa2lb55favhw7mhirylv4pniqu3oxpmoyjbrtfank4kdleiflfqbta"
-    ["ap-osaka-1"]="ocid1.image.oc1.ap-osaka-1.aaaaaaaaevxg6tpchgyijcdjlzcflkat5ndpsfy6n3tjois2qe3yrtrjrnlq"
+    ["ap-osaka-1"]="ocid1.image.oc1.ap-osaka-1.aaaaaaaar7ovad7pwnlsre6fk3ienut5bb522h32uctzxfk3ubwfm5hksx3q"
 )
 
 export CLI_OCI_IMAGE=${CLI_OCI_IMAGEMAP[$OCI_REGION]}
+echo -r "env Declaration... "
 export CLI_OCI_COMPARTMENTID=`oci iam compartment list | jq -r '.data[]."compartment-id"'`
 export CLI_OCI_AD=`oci iam availability-domain list --compartment-id ${CLI_OCI_COMPARTMENTID} | jq -r '.data[].name'`
-
+echo "DONE"
 oci compute instance launch \
     --availability-domain ${CLI_OCI_AD}\
     --compartment-id ${CLI_OCI_COMPARTMENTID} \
@@ -25,4 +26,8 @@ oci compute instance launch \
     --display-name "Wordpress Instance" \
     --user-data-file "init/cloud-config" \
     --ssh-authorized-keys-file ~/.oci/oci-key-public-ssh \
-    --image-id $CLI_OCI_IMAGE 
+    --image-id $CLI_OCI_IMAGE | jq -r '.data[].id'
+
+echo "IPアドレスを取得しています...."
+echo "20秒間そのままお待ち下さい"
+#oci compute instance list-vnics --compartment-id ${CLI_OCI_COMPARTMENTID} --instance-id 
