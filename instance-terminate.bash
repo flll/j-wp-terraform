0b2504f0e08e7be110c95ed2b6755057bf6cbbc3
lll <flll@users.noreply.github.com>
2020-10-30 15:35:01 +0900

    Update instance-terminate.bash

--- instance-terminate.bash
+++ instance-terminate.bash
@@ -9,9 +9,11 @@ export CLI_OCI_AD=`oci iam availability-domain list --compartment-id ${CLI_OCI_C
 export CLI_OCI_INSTANCE_OCID=`oci compute instance list --compartment-id ${CLI_OCI_COMPARTMENTID} --availability-domain ${CLI_OCI_AD} --sort-by TIMECREATED --sort-order DESC --lifecycle-state RUNNING | jq -r '.data[0].id'`
 
 oci compute instance get --instance-id ${CLI_OCI_INSTANCE_OCID} --query 'data.{"名前":"display-name"}' --output table 
-echo 数秒お待ち下さい...
 (
-sleep 3
+echo "削除するインスタンスの確認をお願いします。"
+sleep 1
+echo "数秒お待ち下さい..."
+sleep 2
 echo "\"Y\"[はい] もしくは\"N\"[拒否]のちエンターの入力をお願いします"
 )&
 oci compute instance terminate --instance-id ${CLI_OCI_INSTANCE_OCID} 
