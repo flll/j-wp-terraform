8eecbf8ed4a8ec16701091865b895e07e523eac5
lll <flll@users.noreply.github.com>
2020-11-01 14:32:22 +0900

    Update create-key.bash

--- init/create-key.bash
+++ init/create-key.bash
@@ -1,14 +1,10 @@
 #!/bin/bash
-if [ ! -d ~/.oci ]; then
-    mkdir ~/.oci
-    echo -n "mkdir done  "
-fi
+[ ! -d ~/.oci ] && mkdir ~/.oci
 if [ ! -e ~/.oci/oci-key.pem ]; then
-    echo -n "key-creating...  "
+    echo -n "key-pairing...  "
     openssl genrsa -out ~/.oci/oci-key.pem 2048
     chmod go-rwx ~/.oci/oci-key.pem
     openssl rsa -pubout -in ~/.oci/oci-key.pem -out ~/.oci/oci-key-public.pem
-    ~/.oci/oci-key-public-ssh
     ssh-keygen -f ~/.oci/oci-key-public.pem -i -mPKCS8 > ~/.oci/oci-key-public-ssh
-    echo -n "done  "
+    echo "DONE"
 fi
