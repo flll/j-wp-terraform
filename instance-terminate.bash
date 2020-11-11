1d7c519ce0d8e0fb250ce38f07d583ab9828cd0e
flll <flll@users.noreply.github.com>
2020-11-12 00:35:42 +0900

    staging削除と改行追加

--- instance-terminate.bash
+++ instance-terminate.bash
@@ -4,11 +4,22 @@ set -o pipefail
 echo "削除したいインスタンスは起動しておいてください。"
 echo "一番最近に作成されたインスタンスを削除します。"
 echo "しばらくお待ち下さい..." 
-export CLI_OCI_COMPARTMENTID=`oci iam compartment list | jq -r '.data[]."compartment-id"'`
-export CLI_OCI_AD=`oci iam availability-domain list --compartment-id ${CLI_OCI_COMPARTMENTID} | jq -r '.data[].name'`
-export CLI_OCI_INSTANCE_OCID=`oci compute instance list --compartment-id ${CLI_OCI_COMPARTMENTID} --availability-domain ${CLI_OCI_AD} --sort-by TIMECREATED --sort-order DESC --lifecycle-state RUNNING | jq -r '.data[0].id'`
+export CLI_OCI_COMPARTMENTID=`oci iam compartment list \
+    | jq -r '.data[]."compartment-id"'`
+export CLI_OCI_AD=`oci iam availability-domain list \
+    --compartment-id ${CLI_OCI_COMPARTMENTID} \
+    | jq -r '.data[].name'`
+export CLI_OCI_INSTANCE_OCID=`oci compute instance list \
+    --compartment-id ${CLI_OCI_COMPARTMENTID} \
+    --availability-domain ${CLI_OCI_AD} \
+    --sort-by TIMECREATED \
+    --sort-order DESC \
+    --lifecycle-state RUNNING \
+    | jq -r '.data[0].id'`
 
-oci compute instance get --instance-id ${CLI_OCI_INSTANCE_OCID} --query 'data.{"名前":"display-name"}' --output table 
+oci compute instance get --instance-id ${CLI_OCI_INSTANCE_OCID} \
+    --query 'data.{"名前":"display-name"}' \
+    --output table 
 (
 echo "削除するインスタンスの確認をお願いします。"
 echo "数秒お待ち下さい..."
