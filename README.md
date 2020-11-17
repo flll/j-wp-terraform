0991f28da75645815e482d7334d48e49755d2f75
flll <flll@users.noreply.github.com>
2020-11-18 02:19:46 +0900

    インデント追加

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ region 日本
 ```
 cat << 'EOF' > ~/jt.bash
 #!/bin/bash
-[[ ! -d j-wp/ ]] && git clone https://github.com/flll/j-wp-terraform.git
+[[ ! -d j-wp/ ]] && git clone -q https://github.com/flll/j-wp-terraform.git
 cd j-wp-terraform
 git fetch && git reset --hard origin/main
 chmod 755 -R *
--- init/create-key.bash
+++ init/create-key.bash
@@ -4,7 +4,9 @@ if [ ! -e ~/.oci/oci-key.pem ]; then
     echo -n "key-pairing...  "
     openssl genrsa -out ~/.oci/oci-key.pem 2048
     chmod go-rwx ~/.oci/oci-key.pem
-    openssl rsa -pubout -in ~/.oci/oci-key.pem -out ~/.oci/oci-key-public.pem
-    ssh-keygen -f ~/.oci/oci-key-public.pem -i -mPKCS8 > ~/.oci/oci-key-public-ssh
+    openssl rsa -in  ~/.oci/oci-key.pem -pubout \
+                -out ~/.oci/oci-key-public.pem
+    ssh-keygen -f ~/.oci/oci-key-public.pem -i -mPKCS8 \
+                > ~/.oci/oci-key-public-ssh
     echo "DONE"
 fi
