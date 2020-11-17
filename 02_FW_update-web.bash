b20555159163f4b705dea8dd9903451c1eb18b41
flll <flll@users.noreply.github.com>
2020-11-18 06:11:21 +0900

    functionの追加

--- 02_FW_update-web.bash
+++ 02_FW_update-web.bash
@@ -6,6 +6,7 @@ echo "最も古く作られたoci-VCNのFW-listを置き換えます"
 echo "書き換えてしまうため、追加でFWを設定している場合この処理を行わないでください。"
 echo "キャンセルする場合はctrl + cを入力してください。"
 read -p "続行する場合このままエンターキーを入力してください。" a
+. init/func.bash
 
 oci-get-compartment-ocid
 oci-once-open-http-port
